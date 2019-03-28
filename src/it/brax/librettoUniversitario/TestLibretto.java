23936adfa380cf88ff4c7d0cd31d5a5ba4ed1899
brax <brax@brax-Inspiron-1520>
2019-03-28 22:29:41 +0100

    Punto 4 implementato
    Per verificare se un esame è già inserito nella lista esami del Libretto
    Universitario è stata utilizzata una espressione lambda.
    Per far si che il filtro potesse essere utilizzato in questo punto e nei
    precedenti, il metodo astratto di interfaccia è stato modficato.

--- src/it/brax/librettoUniversitario/TestLibretto.java
+++ src/it/brax/librettoUniversitario/TestLibretto.java
@@ -8,8 +8,9 @@ public class TestLibretto {
 		Libretto libr = new Libretto();
 		Libretto librFiltrato = new Libretto();
 		
-		Filtro votoUgualeA = (Esame e, Voto v, String corso) -> e.getVoto() == v ;
-		Filtro cercaCorso = (Esame e, Voto v, String corso) -> e.getCorso() == corso ;
+		Filtro votoUgualeA = (Esame e, Esame b, Voto v, String corso) -> e.getVoto() == v ;
+		Filtro cercaCorso = (Esame e, Esame b, Voto v, String corso) -> e.getCorso() == corso ;
+		Filtro giaInserito = (Esame e, Esame b, Voto v, String corso) -> e.getCorso() == b.getCorso() && e.getVoto() == b.getVoto() ;
 		
 		libr.add(new Esame("Analisi I", Voto.VENTISEI, LocalDate.of(2000, 1, 13)));
 		libr.add(new Esame("Geometria I", Voto.VENTISETTE, LocalDate.of(2000, 2, 15)));
@@ -17,18 +18,31 @@ public class TestLibretto {
 		libr.add(new Esame("Fisica I", Voto.VENTOTTO, LocalDate.of(2000, 7, 21)));
 		libr.add(new Esame("Fondamenti di Informatica", Voto.TRENTA, LocalDate.of(2000, 9, 15)));
 		
+		/**
+		 * Punto 2: trovare gli esami con una votazione pari ad un certo numero
+		 */
 		for (Esame e : libr.getEsami()) {
-			if (votoUgualeA.filtra(e, Voto.VENTOTTO, null)) librFiltrato.add(e);
+			if (votoUgualeA.filtra(e, null, Voto.VENTOTTO, null)) librFiltrato.add(e);
 		}
 		System.out.println(librFiltrato.getEsami());
-		
 		librFiltrato.clear();
 		
+		/**
+		 * Punto 3: trovare gli esami per il corso uguale a quello che si sta cercando		
+		 */
 		for (Esame e : libr.getEsami()) {
-			if (cercaCorso.filtra(e, null, "Analisi I")) librFiltrato.add(e);
+			if (cercaCorso.filtra(e, null, null, "Analisi I")) librFiltrato.add(e);
 		}
 		System.out.println(librFiltrato.getEsami());
+		librFiltrato.clear();
 		
+		/**
+		 * Punto 4: verificare se un esame risulta già inserito nella lista esami del LibrettoUniversitario
+		 */
+		for (Esame e : libr.getEsami()) {
+			if (giaInserito.filtra(e, new Esame("Chimica", Voto.VENTOTTO, null), null, null))
+				System.out.println(e + " già presente nella lista esami del Libretto Universitario");
+		}
 	}
 
 }
